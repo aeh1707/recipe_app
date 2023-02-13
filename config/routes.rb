3be0b17e8d36cbb4b2f0e27fcb5a447be3a49244
aeh1707d <abdelhadi.hireche@outlook.com>
2023-02-13 16:52:47 +0100

    Updates routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :users do
     resources :recipes, only: [:index, :show]
-    resources :foods
+    resources :foods, only: [:index]
   end                                                                                
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
