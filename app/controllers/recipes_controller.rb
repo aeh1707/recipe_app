7216907dc226becd017c5543e86b357b29ec4ae9
Sajeel Zafar <sajeelzafar1995@gmail.com>
2023-02-15 23:23:38 +0500

    Displaying data of recipes for the selected user

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,7 +3,8 @@ class RecipesController < ApplicationController
 
   # GET /recipes or /recipes.json
   def index
-    @recipes = Recipe.all
+    @user = User.find(params[:user_id].to_i)
+    @recipes = Recipe.where(user: @user)
   end
 
   # GET /recipes/1 or /recipes/1.json
