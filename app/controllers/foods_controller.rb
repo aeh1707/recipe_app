a0fa41a1648a42b2ab076d7be131439bc21bc05c
Sajeel Zafar <sajeelzafar1995@gmail.com>
2023-02-19 00:39:25 +0500

    Clicking on public recipe item would link to the respective detail recipe dish

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -2,7 +2,7 @@ class FoodsController < ApplicationController
   before_action :set_food, only: %i[show edit update destroy]
 
   def index
-    @foods = Food.all
+    @foods = Food.where(user: current_user)
   end
 
   # GET /foods/new
