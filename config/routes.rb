a5c881ff509dc4d656e26405a24146e705200f62
Sajeel Zafar <sajeelzafar1995@gmail.com>
2023-02-18 17:34:03 +0500

    Adding delete functionality in buttons

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   resources :recipes do
     resources :recipe_foods, only: [:new, :create]
-  end                                                                           
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :general_shopping_lists, only: [:index]
+  resources :recipe_foods, only: [:destroy]
   resources :foods, except: [:update]
   # Defines the root path route ("/")
   root "recipes#index"
