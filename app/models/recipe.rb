6f7e9cf63f50f0a89863eca94da358a7af219d4f
Sajeel Zafar <sajeelzafar1995@gmail.com>
2023-02-18 16:23:08 +0500

    Adding missing recipes and there respective foods to shopping list

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -8,7 +8,18 @@ class Recipe < ApplicationRecord
   belongs_to :user
   has_many :foods, through: :recipe_foods
 
-  def total_price
-    total = 0
+  def missing_foods
+    data = { list: [], count: 0, total_price: 0 }
+    recipe_foods.includes(:food).each do |recipe_food|
+      recipe_food_quantity = recipe_food.quantity
+      food_available_quantity = recipe_food.food.quantity
+
+      next unless (food_available_quantity.to_i - recipe_food_quantity.to_i).negative?
+
+      data[:list] << recipe_food.food
+      data[:count] += 1
+      data[:total_price] += recipe_food.food.price
+    end
+    data
   end
 end
